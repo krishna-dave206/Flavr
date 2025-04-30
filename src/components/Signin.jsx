d88a496ebb071e969d03f274a2d15ea0ce94f13e
Krishna Dave <krishna.dave2024@nst.rishihood.edu.in>
2025-04-30 10:30:53 +0530

    hello

---
+++ src/components/Signin.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Signin.css'
+
+const Signin = () => {
+  return (
+    <div className='sign-in-body'>
+        <div className='sign-in-container'>
+
+        </div>
+
+        <div className='left-panel'>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Signin
