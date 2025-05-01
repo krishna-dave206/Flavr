d02786ee9c418434f2fa186ace14c6143bd6a830
Krishna Dave <krishna.dave2024@nst.rishihood.edu.in>
2025-05-01 21:15:59 +0530

    added the login page

---
+++ src/components/Login.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import image from '../assets/left-panel-img.jpg'
+import Signin from './Signin'
+const Login = () => {
+    return (
+        <div className='sign-in-body'>
+          <div className='left-panel'>
+          <h1>Flavr</h1>
+          <img src={image} alt="" />
+          <p>Cook. Share. Connect</p>
+    
+          </div>
+            <div className='sign-in-container'>
+              <h2>Flavr</h2>
+              <p>Cook. Share. Connect.</p>
+    
+              <label>Email</label>
+              <br />
+              <input type="email" />
+              <br />
+    
+              <label htmlFor="">Password</label>
+              <br />
+              <input type="password"/>
+              <br />
+    
+              <label htmlFor="">Confirm Password</label>
+              <br />
+              <input type="password"/>
+              <br />
+    
+              <button>Login</button>
+                <p className='login'>Don't have an Account? <span>Sign Up</span></p>
+            </div>
+    
+        </div>
+      )
+}
+
+export default Login
--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,16 +1,55 @@
-import React from 'react'
+import React, {useRef, useState} from 'react'
 import './Signin.css'
+import img from '../assets/left-panel-img.jpg'
+import Login from './Login.jsx'
+
 
 const Signin = () => {
+  const password1 = useRef();
+  const password2 = useRef();
+  const [click, setClick]  = useState(false)
+  if (click){
+    return <Login />
+  }
   return (
     <div className='sign-in-body'>
+      <div className='left-panel'>
+      <h1>Flavr</h1>
+      <img src={img} alt="" />
+      <p>Cook. Share. Connect</p>
+
+      </div>
         <div className='sign-in-container'>
-          <div><h1></h1></div>
+          <h2>Flavr</h2>
+          <p>Cook. Share. Connect.</p>
 
-        </div>
+          <label>Email</label>
+          <br />
+          <input type="email" />
+          <br />
 
-        <div className='left-panel'>
+          <label htmlFor="">Password</label>
+          <br />
+          <input type="password" ref={password1}/>
+          <br />
 
+          <label htmlFor="">Confirm Password</label>
+          <br />
+          <input type="password" ref={password2}/>
+          <br />
+
+          <button onClick={()=>{
+            if(password1.current.value === password2.current.value){
+              console.log("Signup successful") 
+
+            }
+            else{
+              alert("The passsword entered should be same. Check again.")
+            }
+          }}>Create Account</button>
+            <p className='login'>Already have an Account? <span onClick={
+              ()=>{setClick(true)}
+            }>Login</span></p>
         </div>
 
     </div>
